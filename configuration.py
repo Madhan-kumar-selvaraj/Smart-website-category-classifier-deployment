873ee6287a8d09919bb6c7b02a52e8b4ba247d18
Madhan Kumar Selvaraj <madhanprimary@gmail.com>
2020-07-03 11:23:09 +0530

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  1 18:04:26 2020
+
+@author: Madhan KUmar Selvaraj
+"""
+
+input_file_path = r"input.txt"
+output_file_path = r"output.txt"
+
+vocabulary_path = r"vocabulary.pkl"
+classifier_model_path = r"classifier_model.pkl"
+
+website_category_path = r"site_category.json"
