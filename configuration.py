bf0aeb28bf0af615c6e97a904b91cfc750b11090
Madhan Kumar Selvaraj <madhanprimary@gmail.com>
2020-07-03 11:06:02 +0530

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -5,10 +5,10 @@ Created on Wed Jul  1 18:04:26 2020
 @author: Madhan KUmar Selvaraj
 """
 
-input_file_path = r"input_colab.txt"
-output_file_path = r"output_colab.txt"
+input_file_path = r"./tmp/input_colab.txt"
+output_file_path = r"./tmp/output_colab.txt"
 
-vocabulary_path = r"vocabulary.pkl"
-classifier_model_path = r"classifier_model.pkl"
+vocabulary_path = r"./tmp/vocabulary.pkl"
+classifier_model_path = r"./tmp/classifier_model.pkl"
 
-website_category_path = r"site_category.json"
+website_category_path = r"./tmp/site_category.json"
