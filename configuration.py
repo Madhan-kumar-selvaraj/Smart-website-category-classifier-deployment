e0e0fba8542e3a4aae588fbfe9eb18b9998419f3
Madhan Kumar Selvaraj <madhanprimary@gmail.com>
2023-06-19 17:46:42 +0530

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -11,4 +11,7 @@ output_file_path = r"output.txt"
 vocabulary_path = r"vocabulary.pkl"
 classifier_model_path = r"classifier_model.pkl"
 
+vocabulary_backup_path = r"vocabulary_backup.pkl"
+classifier_model_backup_path = r"classifier_backup_model.pkl"
+
 website_category_path = r"site_category.json"
